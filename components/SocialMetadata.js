0fc9a389531e85480b0eda4be9ea8f35cbb87575
Nami Sunami <naoyuki.sunami@gmail.com>
2022-05-05 21:16:12 -0400

    Add social image

---
+++ components/SocialMetadata.js
@@ -0,0 +1,34 @@
+import React from "react";
+import SocialImage from "../public/social-image.png";
+
+export const SocialMetadata = () => {
+  return (
+    <>
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary" key="twcard" />
+      <meta name="twitter:creator" content={"@n_sunami"} key="twhandle" />
+      {/* Open Graph */}
+      <meta
+        property="og:url"
+        content={"https://www.naoyukisunami.com/"}
+        key="ogurl"
+      />
+      <meta property="og:image" content={SocialImage} key="ogimage" />
+      <meta
+        property="og:site_name"
+        content={"Naoyuki Sunami"}
+        key="ogsitename"
+      />
+      <meta
+        property="og:title"
+        content={"Naoyuki Sunami's Portfolio"}
+        key="ogtitle"
+      />
+      <meta
+        property="og:description"
+        content={"Behavioral Data Scientist"}
+        key="ogdesc"
+      />
+    </>
+  );
+};
