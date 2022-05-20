db5623d5138d66173d9d09a2c1bb137e697cc7dd
Nami Sunami <naoyuki.sunami@gmail.com>
2022-05-19 21:30:09 -0400

    Change title for social metadata

--- components/SocialMetadata.js
+++ components/SocialMetadata.js
@@ -3,14 +3,14 @@ import React, { useEffect } from "react";
 export const SocialMetadata = () => {
   return (
     <>
-      <meta name="description" content="Behavioral Data Scientist" />
+      <meta name="description" content="Data Expert/Software Dev" />
 
       {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:domain" content="naoyukisunami.com" />
       <meta property="twitter:url" content="https://www.naoyukisunami.com/" />
       <meta name="twitter:title" content="Naoyuki Sunami's Portfolio" />
-      <meta name="twitter:description" content="Behavioral Data Scientist" />
+      <meta name="twitter:description" content="Data Expert/Software Dev" />
       <meta
         name="twitter:image"
         content="https://naoyukisunami.com/social-image.jpg"
@@ -39,7 +39,7 @@ export const SocialMetadata = () => {
       />
       <meta
         property="og:description"
-        content={"Behavioral Data Scientist"}
+        content={"Data Expert/Software Dev"}
         key="ogdesc"
       />
     </>
